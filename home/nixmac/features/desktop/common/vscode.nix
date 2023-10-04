df6412dcbe2db5aa2fbb755fa9d5aba83504716c
macsiwase <github.impairers@simplelogin.co>
2023-10-04 02:52:42 +0200

    update

--- home/nixmac/features/desktop/common/vscode.nix
+++ home/nixmac/features/desktop/common/vscode.nix
@@ -1,9 +1,15 @@
 { pkgs, ... }: {
-  programs.vscode.package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
-    src = (builtins.fetchTarball {
-      url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-      sha256 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula 
+    ];
+    package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
+      src = (builtins.fetchTarball {
+        url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+        sha256 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+      });
+      version = "latest";
     });
-    version = "latest";
-  });
+  };
 }
