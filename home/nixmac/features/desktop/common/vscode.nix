8a3719a66307cc999b75f419b2424fa1bba8e9de
macsiwase <github.impairers@simplelogin.co>
2023-10-04 02:45:02 +0200

    update

--- home/nixmac/features/desktop/common/vscode.nix
+++ home/nixmac/features/desktop/common/vscode.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   programs.vscode.package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
     src = (builtins.fetchTarball {
       url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
