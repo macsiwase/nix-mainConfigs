78332a399ae8f0c61fbe1b09ee97ee29a14faf7d
macsiwase <github.impairers@simplelogin.co>
2023-10-04 03:03:30 +0200

    update

--- home/nixmac/features/desktop/common/vscode.nix
+++ home/nixmac/features/desktop/common/vscode.nix
@@ -7,7 +7,7 @@
     package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
       src = (builtins.fetchTarball {
         url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-        sha256 = "eca26510954a9196c14728b3347872dcf472224c743b67cf2b73ce0028fcc1de";
+        sha256 = "1wkrw5vva22wgsnjld06nj4y3cqirx9273v3mh0zzl9mcvmv4f6m";
       });
       version = "latest";
     });
